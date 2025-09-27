8371be991a122fbc22086147dae678516aabdb74
C3LMQ9 <zahorayanna@gmail.com>
2025-09-27 10:26:11 +0200

    2.alkalom

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
-# Ez az első módosítás
+# 2.alkalom
+import random
+from importlib.metadata import pass_none
+from types import new_class
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+'''szam = input("Kérek egy számot: ")
+if szam.isdigit():
+    szam = int(szam)
+    if szam > 0:
+        print("Pozitív")
+    elif szam < 0:
+        print("Negatív")
+    else:
+        print("Nulla")
+else:
+    print("Nem számot adtál meg.")'''
 
+'''jegy = int(input("Kérek egy érdemjegyet: "))
+while jegy == 1 or jegy > 5 or jegy < 1:
+    jegy = int(input("Kérek egy érdemjegyet: "))
+    if jegy == 10:
+        print ("Csillagos ötös!")
+        break
+else:
+    print("Jól teljesített.")
+print ("Gratulálunk")'''
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+'''kocka = random.randint(1,6)
+while True:
+    tipp = int(input("Kérek egy tippet: "))
+    if tipp == kocka:
+        break
+print ("Sikerült eltalálnod a számot!")'''
 
+'''uzenet = "Jó reggelt"
+for betu in uzenet:
+    print(betu)
+    if betu == " ":
+        break'''
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+'''for _ in range(5):
+    print("Nem leszek többet rossz.")
+for elem in range(1, 4):
+    print(elem, "Soha nem leszek többet rossz.")'''
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+'''try:
+    print(10/0)
+except ZeroDivisionError:
+    print("HIBA! Nullával osztás.")
+except NameError:
+    print("HIBA! Névhiba.")
+print("OK")'''
+
+while True:
+    try:
+        szam = int(input("Kérek egy egész számot: "))
+    except:
+        print("Nem egész számot adott meg!")
+    else:
+        break
